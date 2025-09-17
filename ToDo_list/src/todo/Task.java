a515dab557a9640b1596f8ce68c4b897b3cd778d
kacperchlastawa <kacper.chlastawa03@gmail.com>
2025-09-17 20:05:10 +0200

    Implementing task class

--- ToDo_list/src/todo/Task.java
+++ ToDo_list/src/todo/Task.java
@@ -1,16 +1,28 @@
 public class Task{
 
 
-    public int id;
-    public String description;
+    private  final int id;
+    private  String description;
 
     public Task(int id, String description) {
         this.id = id;
         this.description = description;
     }
-
+    @Override
     public String toString() {
         return "Task ID: " + id + ", Description: " + description;
     }
+    public int getId() {
+        return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    // public void setId(int id){
+    //     this.id = id;
+    // }
 
 }
