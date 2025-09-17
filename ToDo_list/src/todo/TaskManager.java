9ad145d21773e11122298e481fa69e75055aeea1
kacperchlastawa <kacper.chlastawa03@gmail.com>
2025-09-17 20:01:44 +0200

    Implementing some methods

--- ToDo_list/src/todo/TaskManager.java
+++ ToDo_list/src/todo/TaskManager.java
@@ -1,14 +1,31 @@
-public class TaskManager {
+
+import java.util.ArrayList;
+
+public class TaskManager{
+        private int nextId = 1;
+        private ArrayList<Task> tasks = new ArrayList<>();
 
     public void addTask(String description) {
-        // Implementation for adding a task
+      
+        Task newTask = new Task(nextId++, description);
+        tasks.add(newTask);
     }
     
     public void removeTask(int taskId) {
-        // Implementation for removing a task
-    }
+        int sizeBefore = tasks.size();
+        tasks.removeIf(task -> task.getId() == taskId);
+        int sizeAfter = tasks.size();
+        String ifnotremoved = (sizeBefore == sizeAfter) ?
+        "Task with ID " + taskId + " not removed. There was a problem" : "Task with ID " + taskId + " removed successfully.";
+        System.out.println(ifnotremoved);
+    }   
     public void listTasks() {
-        // Implementation for listing all tasks
+        if(tasks.isEmpty()){
+            System.out.println("No tasks available.");
+        }
+        for(Task task : tasks){
+            System.out.println(task);
+        }
     }
     public void saveToFile(String filename) {
         // Implementation for saving tasks to a file
